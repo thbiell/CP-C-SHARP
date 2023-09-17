5a357c2897cdb69b51d76fcaf40a65e84737dc51
thbiell <thiago007gabriel@gmail.com>
2023-09-17 18:03:28 -0300

    a

--- Data/DTOs/ReadPokemonDto.cs
+++ Data/DTOs/ReadPokemonDto.cs
@@ -4,10 +4,11 @@ namespace CheckpointAPI.Data.DTOs;
 
 public class ReadPokemonDto
 {
-    public int Id { get; set; }
-    public string Nome { get; set; }
+  public int Id { get; set; }
+  public string Nome { get; set; }
   public int Nivel { get; set; }
   public float Exp { get; set; }
   public TipoPokemon Tipo { get; set; }
+  public string TipoNome => Tipo.ToString();
 
 }
